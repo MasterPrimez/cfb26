2ff0ea6346a8f70b1ceed8cb1e7a89573b6684a9
MasterPrimez <michael.stine@gmail.com>
2026-09-06 00:29:34 -0700

    Cloudflare accounts live

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 // Filled in by tools/cf-setup.sh after the Worker is deployed. Empty = accounts are off (app works on-device only).
-export const API_URL = '';
+export const API_URL = 'https://cfb26-api.michael-stine.workers.dev';
